2e64a823337ad6971eab271a07dd2aca2b906458
CreaSoley <artcreatifmeg@gmail.com>
2026-02-10 10:46:57 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,27 @@
-const CACHE_NAME = "routines-v1";
+const CACHE_NAME = "ceralune-v1";
+const OFFLINE_URL = "./offline.html";
+
 const ASSETS = [
   "./",
   "./index.html",
+  "./offline.html",
   "./style.css",
   "./app.js",
-  "./manifest.json",
+  "./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then(response => {
+        return response || caches.match(OFFLINE_URL);
+      });
+    })
   );
 });
-
