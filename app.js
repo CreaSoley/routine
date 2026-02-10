830cea0524340e9f859ba7b047389b50fc9f369c
CreaSoley <artcreatifmeg@gmail.com>
2026-02-10 09:16:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 
+document.getElementById("notif").onclick = async () => {
+  const permission = await Notification.requestPermission();
+  if (permission === "granted") {
+    alert("Rappels activés !");
+  }
+};
